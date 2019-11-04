5c4f2a15cf69ef86a5f2c68ec37394d1f29ca5ad
bemcgarvey <bemcgarvey@comcast.net>
2019-11-04 11:07:09 -0700

    Runcam Remote code added

--- firmware/RuncamRemote.X/main.c
+++ firmware/RuncamRemote.X/main.c
@@ -43,10 +43,12 @@
 
 #define _XTAL_FREQ   1000000U
 
-//define rx pulse length in microseconds required to turn on output switch.
+//define rx pulse length in microseconds required to turn on output.
 #define ON_US       1600U
-//define rx pulse length in microseconds required to turn off output switch.
+//define rx pulse length in microseconds required to turn off output.
 #define OFF_US      1400U
+//define double toggle time in ms
+#define DOUBLE_TOGGLE_TIME      500
 
 //calculation of Timer1 values
 #define ON_PULSE    (ON_US / 4)
@@ -56,9 +58,16 @@ void configPins(void);
 void configTimer1(void);
 void startTimer1(void);
 void configTimer2(void);
-void startTimer2(void);
 void configInterrupts(void);
 void configPMD(void);
+void pulse(char pulses);
+
+typedef enum {INPUT_UNKNOWN, INPUT_ON, INPUT_OFF} InputState;
+volatile InputState inputState = INPUT_UNKNOWN;
+volatile bool toggled = false;
+typedef enum {PULSE_LOW, PULSE_HIGH} PulseState;
+volatile PulseState pulseState = PULSE_LOW;
+volatile char pulseCount;
 
 void main(void) {
     configPins();
@@ -67,10 +76,26 @@ void main(void) {
     configTimer2();
     configInterrupts();
     startTimer1();
-    startTimer2();
     while (1) {
-        
+        if (toggled) {
+            toggled = false;
+            __delay_ms(DOUBLE_TOGGLE_TIME);
+            if (toggled) {
+                pulse(2);
+            } else {
+                pulse(1);
+            }
+            toggled = false;
+        }
     }
+}
+
+void pulse(char pulses) {
+    pulseCount = (pulses == 1) ? 1 : 3;
+    pulseState = PULSE_HIGH;
+    LATAbits.LATA2 = 1;
+    TMR2 = 0;
+    T2CONbits.TMR2ON = 1;
 }
 
 void configPins(void) {
@@ -100,12 +125,10 @@ void startTimer1(void) {
 }
 
 void configTimer2(void) {
-    TMR2 = 0;
-    PR2 = 250; 
-}
-
-void startTimer2(void) {
-    T2CONbits.TMR2ON = 1;
+    //Set Timer2 for 80ms period.
+    PR2 = 125;
+    T2CONbits.T2CKPS = 0b10; //1:16 prescale
+    T2CONbits.T2OUTPS = 0b1001; //1:10 postscale
 }
 
 void configInterrupts(void) {
@@ -118,6 +141,7 @@ void configInterrupts(void) {
 }
 
 void configPMD(void) {
+    //Turn off everything except Timer1 and Timer2 and PCLCK
     PMD0 = 0b01000111;
     PMD1 = 0b10000001;
     PMD2 = 0b01100110;
@@ -128,15 +152,31 @@ void configPMD(void) {
 
 void __interrupt() isr(void) {
     if (PIR1bits.TMR2IF == 1) {
-        
+        if (pulseState == PULSE_HIGH) {
+            LATAbits.LATA2 = 0;
+            pulseState = PULSE_LOW;
+        } else {
+            LATAbits.LATA2 = 1;
+            pulseState = PULSE_HIGH;
+        }
+        --pulseCount;
+        if (pulseCount == 0) {
+            T2CONbits.TMR2ON = 0;
+        }
         PIR1bits.TMR2IF = 0;
     }
     if (PIR1bits.TMR1GIF == 1) {
         if (TMR1 <= OFF_PULSE) {
-            
+            if (inputState == INPUT_ON) {
+                toggled = true;
+            }
+            inputState = INPUT_OFF;
         }
-        if (TMR1 >= ON_PULSE) {
-            
+        else if (TMR1 >= ON_PULSE) {
+            if (inputState == INPUT_OFF) {
+                toggled = true;
+            }
+            inputState = INPUT_ON;
         }
         TMR1L = 0;
         TMR1H = 0;
